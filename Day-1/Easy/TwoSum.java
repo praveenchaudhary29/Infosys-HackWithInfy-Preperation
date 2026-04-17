272b9f2cad094b61b09f638c2f1bc4ab3d9f7057
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-17 10:44:19 +0530

    Create TwoSum.java

---
+++ Day-1/Easy/TwoSum.java
@@ -0,0 +1,15 @@
+//Question- Leetcode(1 Easy)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+       for(int i = 0; i<nums.length; i++){
+           int element = target - nums[i];
+           if(map.containsKey(element)){
+            return new int[]{i,map.get(element)};
+           }
+           map.put(nums[i],i);
+       }
+    return new int[]{};
+    }
+}
