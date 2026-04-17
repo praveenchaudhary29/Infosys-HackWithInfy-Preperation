f7aefd6c94dbf33a0208099c4f82c7d0fc92d609
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-17 12:41:56 +0530

    Create Contains-Duplicate.java

---
+++ Day-1/Easy/Contains-Duplicate.java
@@ -0,0 +1,12 @@
+//Question - Leetcode(217 Easy)
+//Here use HashSet,when set already contains a value and you try adding the same value again it returns false
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+      Set<Integer> count = new HashSet<>();
+      for(int num : nums){
+        if(!count.add(num)) return true;
+      }
+    return false;    
+    }
+}
