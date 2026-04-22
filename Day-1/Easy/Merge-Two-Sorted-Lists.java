2950782d54e99653adb35405e18bacb061c6dd59
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-22 21:58:05 +0530

    Update Merge-Two-Sorted-Lists.java

--- Day-1/Easy/Merge-Two-Sorted-Lists.java
+++ Day-1/Easy/Merge-Two-Sorted-Lists.java
@@ -3,7 +3,7 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         //edge cases
-        if(list1==null) return list2;
+        if(list1==null)  return list2;
         if(list2==null) return list1;
         if(list1==null && list2==null) return list1;
 
